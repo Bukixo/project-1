1c668b1b4659fb2121450399ec933fedbb891433
Buki <Bukithompson@gmail.com>
2017-02-11 10:13:44 +0000

    added functions to buttons

--- js/app.js
+++ js/app.js
@@ -17,10 +17,43 @@ $(() => {
     // if they are generate a random option (red blue green yellow)
     // push it into cpuArray
     // clear the playerArray
+    
+
   });
 
-  
+  // $blue.on('click',function (e){
+  //  const $blue =(e.target);
+  //   console.log('blue');
+  // });
+  //
+  // $red.on('click', function(e) {
+  //   const $red = (e.target);
+  //   console.log('red');
+  // });
+  //
+  // $green.on('click', function(e) {
+  //   const green = (e.target);
+  //   console.log('green');
+  // });
+  //
+  // $yellow.on('click', function(e) {
+  // const $yellow = (e.target);
+  //   console.log('yellow');
+  // });
 
+  var running = (playerArray.length === cpuArray.length);
+
+  while (running === true) {
+    if (playerArray.join === cpuArray.join){
+      console.log(playerArray);
+      //generate random option to push on to cpuArray
+      //clear playerArray
+      //assign or
+    } else {
+      //Generate random option to push on to cpuArray
+      alert('Oh no its wrong!');
+    }
+  }
 
   // const sequence = [];
   //
